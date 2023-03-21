c8e0dc480d5f3d1abb5a5f3d3d93cdb8023852fd
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2023-03-21 16:30:06 -0300

    Clases ECMAScript y ECMAScript avanzado corregido

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ class ProductManager {
         // Validate that all fields are required
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log("Error: All the fields are required.");
+            return; // Don't add the product
         }
 
     // Validate that the "code" field is not repeated
     const existingProduct = this.products.find((product) => product.code === code);
     if (existingProduct) {
         console.log(`Error: There is already a product with the code ${code}.`);
+        return; // Don't add the product
     }
 
     // Add product to the array
@@ -48,14 +50,16 @@ class ProductManager {
 
 /* const productManager = new ProductManager();
 
-productManager.addProduct("producto prueba", "Esto es un producto prueba", 200, "Sin imagen", "abc123", 25);
-productManager.addProduct("producto prueba", "Esto es un producto prueba", 200, "Sin imagen", "abc123", 25);
+productManager.addProduct("producto prueba", "Esto es un producto prueba", 200, "Sin imagen", "abc123",25);
+productManager.addProduct("producto prueba", "Esto es un producto prueba", 200, "Sin imagen", "abc123",25);
 
 
 const allProducts = productManager.getProducts();
+console.log("These are all the products:");
 console.log(allProducts);
 
 const productById = productManager.getProductById(1);
+console.log("This is the product that you are looking for:");
 console.log(productById); */
 
 
